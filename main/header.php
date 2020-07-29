9c035c6696f1b0160217806943ae832ebae97f3f
Mohamed Hefny <mohhef@rocketmail.com>
2020-07-29 18:10:21 -0400

    posting job page

---
+++ main/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta http-equiv="x-ua-compatible" content="IE=edge">
+    <script src="js/jquery3.4.1.min.js"></script>
+    <script src="js/1.14.7popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
+    <script src="js/bootbox.min.js"></script>
+    <script src="js/bootbox.locales.min.js"></script>
+
+    <link rel="stylesheet" href="css/4.3.1bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/find_a_job.css" />
+    <link rel="stylesheet" type="text/css" href="../../inventory/style/main.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
+</head>
+
+<body>
+<div class="row">
+    <div class="col-md-4 mr-auto" id="change">
+      <a href="../../inventory/main.php">
+        <img src="png/findAjob.png" width=auto height=70px alt="" width=auto height=50px id ="Logo" position:absolute;>
+      </a>
+    </div>
+    <div class="col-md-1-auto" id="change1">
+      <a class="btn btn-outline-primary" href="post_job.php" id="pipeview">Home</a>
+    </div>
+    <div class="col-md-1-auto" id="change1">
+      <a class="btn btn-outline-primary" href="../../inventory/insert.php" id="pipeview">Post A job</a>
+    </div>
+    <div class="col-md-1-auto" id="change1">
+      <a class="btn btn-outline-primary" href="../../inventory/modify.php" id="pipeview">Modify A job</a>
+    </div>
+    <div class="col-md-1-auto" id="change2">
+    </div>
+  </div>
