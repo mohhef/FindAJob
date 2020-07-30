2ed43dae4fdddc5481908cad0be4450d37e1e7dc
Robert <robertbeaudenon@gmail.com>
2020-07-30 18:03:29 +0300

    Added general styling in contact page

--- main/header.php
+++ main/header.php
@@ -21,6 +21,17 @@
       <script src="js/ColReorderWithResize.js"></script>
       <script src="js/bootbox.min.js"></script>
       <script src="js/bootbox.locales.min.js"></script>
+    <?php } 
+    elseif(basename($_SERVER['PHP_SELF']) == 'contact.php'){
+      ?>
+      <script src="js/jquery-3.31.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/jquery.dataTables.min.js"></script>
+      <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+      <script src="js/dataTables.bootstrap4.min.js"></script>
+      <script src="js/ColReorderWithResize.js"></script>
+      <script src="js/bootbox.min.js"></script>
+      <script src="js/bootbox.locales.min.js"></script>
     <?php } ?>    
     
 
@@ -40,7 +51,15 @@
       <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
       <!--  <link rel="stylesheet" type="text/css" href="../../inventory/style/main.css" /> -->
       <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" />      
-    <?php } ?>    
+    <?php } 
+     elseif(basename($_SERVER['PHP_SELF']) == 'contact.php'){
+      ?>
+      <link rel="stylesheet" href="css/4.1.3bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="css/maindata.css" />
+      <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
+      <!--  <link rel="stylesheet" type="text/css" href="../../inventory/style/main.css" /> -->
+      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" />      
+    <?php }?>    
 </head>
 
 <body>
