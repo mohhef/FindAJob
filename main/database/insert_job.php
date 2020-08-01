adb8aeed74c7065fa30feac5598c1c2db6c3972d
Mohamed <mohhef@gmail.com>
2020-08-01 14:20:10 -0400

    associate jobs with employees

--- main/database/insert_job.php
+++ main/database/insert_job.php
@@ -5,18 +5,21 @@ $emp_no=$_POST['emp_no'];
 $category_sel=$_POST['category_sel'];
 $description_box=$_POST['description_box'];
 $date_posted = date("Y-m-d");
+$user_name=$_COOKIE['employer_username'];
 
 if ($post_job==true){
-    insertJob($job_title,$emp_no,$description_box,$date_posted,$category_sel);
+    insertJob($job_title,$emp_no,$description_box,$date_posted,$category_sel,$user_name);
 }
 
 
-function insertJob($job_title,$emp_no,$description_box,$date_posted,$category_sel){
+function insertJob($job_title,$emp_no,$description_box,$date_posted,$category_sel,$user_name){
     require('config.php');
     $query1 ="INSERT INTO job (title,employee_needed,description,date_posted,category) VALUES ('$job_title','$emp_no','$description_box','$date_posted','$category_sel')";
     $query2 ="INSERT INTO applies (date_applied,application_status) VALUES ('','')";
+    $query3 ="INSERT INTO post (user_name) VALUES ('$user_name')";
     $result = mysqli_query($conn, $query1);
-    $result = mysqli_query($conn, $query2);
+    $result2 = mysqli_query($conn, $query2);
+    $result3 = mysqli_query($conn, $query3);
     if(true)
     {
         echo 'Data Inserted';
