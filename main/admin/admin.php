b6b85f7d2fd278533530c40bc80213372fe75150
Mohamed Hefny <mohhef@rocketmail.com>
2020-08-02 16:00:24 +0300

    Added table in admin page to check outstanding accounts(need to verify how to check date)

--- main/admin/admin.php
+++ main/admin/admin.php
@@ -43,6 +43,23 @@
   </div>
 </div>
 
+<div class="container-fluid" id="rcorners1">
+  <div class="table-responsive">
+    <div id="alert_message"></div>
+    <table id="outstanding_data" class="display nowrap table table-borderless table-striped" style="" >
+    <h2 class="text-center">Outstanding balance account</h2> 
+    <thead>
+        <tr>
+          <th>user_name</th>
+          <th>email</th>
+          <th>balance</th>
+          <th>since</th>   
+        </tr>
+      </thead>
+    </table>
+  </div>
+</div>
+
 <nav id="nav" class="navbar navbar-expand-sm  navbar-dark">
 </nav>
 <?php include "footer.php" ?>
@@ -138,6 +155,45 @@ $(document).ready(function(){
     },
     "ajax" : {
       url:"../database/fetch_employee.php",
+      type:"POST"
+    }
+  });
+
+       var dataTable = $('#outstanding_data').DataTable({
+        "bootstrap": true,
+      "sDom": "Rlfrtip",
+      "bAutoWidth":false,
+      "responsive":true,
+      "scrollCollapse": true,
+              "searching": false,
+
+      "processing":true,
+      "serverSide":true,
+      "aLengthMenu": [[10, 25, 30, -1], [10, 25, 30, "All"]],
+      "pageLength": 25,
+      "bInfo":false,
+      //    "order" : [],
+      "columns": [{
+        "orderable": true,
+        // "width":"25px"
+
+      }, {
+        "orderable": true,
+        //  "width":"15%"
+      }, {
+        "orderable": true,
+        //  "width":"15%"
+      }, {
+        "orderable": true,
+        //    "width":"21%"
+      }
+    ],
+    "language": {
+      "sSearch": "  ",
+      "searchPlaceholder": "search"
+    },
+    "ajax" : {
+      url:"../database/fetch_outstanding_account.php",
       type:"POST"
     }
   });
