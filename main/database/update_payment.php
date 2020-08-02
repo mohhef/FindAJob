98e2f812be0837eff7481412088667ad2e615675
Caren Rizk <karen.rizk1997@gmail.com>
2020-08-02 18:19:59 -0400

    Fixed some bugs

--- main/database/update_payment.php
+++ main/database/update_payment.php
@@ -4,12 +4,15 @@ updatePayment();
 function updatePayment() {
     require('config.php');
     $user = '';
+    $type = '';
     if (isset($_COOKIE['employer_username'])) {
+        $type = "employer";
         $user = $_COOKIE['employer_username'];
     } else if (isset($_COOKIE['employee_username'])) {
         $user = $_COOKIE['employee_username'];
+        $type = "employee";
     }
-    $query1 = "UPDATE `employer` SET preferred_method ='".$_POST['id']."' WHERE user_name = '$user'";
+    $query1 = "UPDATE `$type` SET preferred_method ='".$_POST['id']."' WHERE user_name = '$user'";
     $result = mysqli_query($conn, $query1);
     if(true)
     {
