7453f066240fe51da9597b074dd952435c5a545c
Caren Rizk <karen.rizk1997@gmail.com>
2020-08-03 19:59:12 -0400

    Fixed some bugs and added automatic/manual to chequing

--- main/database/update_chequing.php
+++ main/database/update_chequing.php
@@ -1,8 +1,16 @@
 <?php
     require('config.php');
+    $user = '';
+      if (isset($_COOKIE['employer_username'])) {
+          $type = "loyer_credit_pays";
+      } else if (isset($_COOKIE['employee_username'])) {
+          $type = "loyee_credit_pays";
+      }
 
-    $updateQuery= "UPDATE `chequing` SET bank_no = '".$_POST["bank_no"]."',
+    $updateQuery1= "UPDATE `chequing` SET bank_no = '".$_POST["bank_no"]."',
     transit_no =  '".$_POST["transit_no"]."' WHERE account_no = '".$_POST["account_no"]."'";
+
+    $updateQuery2 = "UPDATE `$type` SET `automatic_manual`='".$_POST["automatic_manual"]."' WHERE account_no = '".$_POST["account_no"]."'";
 
     mysqli_query($conn, $updateQuery);
     // mysqli_query($conn, $updateQuery2);
