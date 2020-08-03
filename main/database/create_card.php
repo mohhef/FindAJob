2ca5739a39e534f8248e3e7df3f9f975377053d6
Caren Rizk <karen.rizk1997@gmail.com>
2020-08-03 19:59:12 -0400

    Fixed some bugs and added automatic/manual to chequing

--- main/database/create_card.php
+++ main/database/create_card.php
@@ -11,15 +11,17 @@ if ($post_card==true){
 
 
 function insertChequing($card_no,$name,$expiration_date, $automatic_manual){
+    require('config.php');
     $user = '';
     if (isset($_COOKIE['employer_username'])) {
         $user = $_COOKIE['employer_username'];
+        $type = "loyer_credit_pays";
     } else if (isset($_COOKIE['employee_username'])) {
         $user = $_COOKIE['employee_username'];
+        $type = "loyee_credit_pays";
     }
-    require('config.php');
     $query1 ="INSERT INTO card_method (card_number,name,expiration_date) VALUES ('$card_no','$name','$expiration_date')";
-    $query2 ="INSERT INTO loyer_credit_pays (`user_name`,card_number, `automatic_manual`) VALUES ('$user', $card_no, '$automatic_manual')";
+    $query2 ="INSERT INTO `$type` (`user_name`,card_number, `automatic_manual`) VALUES ('$user', $card_no, '$automatic_manual')";
     $result = mysqli_query($conn, $query1);
     $result = mysqli_query($conn, $query2);
     if(true)
