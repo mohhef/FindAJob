b3a537c64e3309da07bbdeea047caa5906357160
Mohamed Hefny <mohhef@rocketmail.com>
2020-08-03 19:59:12 -0400

    Fixed some bugs and added automatic/manual to chequing

--- main/database/delete_card.php
+++ main/database/delete_card.php
@@ -3,11 +3,19 @@ require('config.php');
 
 if(isset($_POST["card_number"]))
 {
- $query = "DELETE FROM loyer_credit_pays WHERE card_number = '".$_POST["card_number"]."'";
- if(mysqli_query($conn, $query))
- {
-  echo 'data deleted!';
- }
+    $user = '';
+    if (isset($_COOKIE['employer_username'])) {
+        $user = $_COOKIE['employer_username'];
+        $type = "loyer_credit_pays";
+    } else if (isset($_COOKIE['employee_username'])) {
+        $user = $_COOKIE['employee_username'];
+        $type = "loyee_credit_pays";
+    }
+    $query = "DELETE FROM `$type` WHERE card_number = '".$_POST["card_number"]."' AND user_name='$user'";
+    if(mysqli_query($conn, $query))
+    {
+    echo 'data deleted!';
+    }
 }
 
 ?>
--- main/database/delete_chequing.php
+++ main/database/delete_chequing.php
@@ -3,11 +3,19 @@ require('config.php');
 
 if(isset($_POST["account_no"]))
 {
- $query = "DELETE FROM loyer_chequing WHERE account_no = '".$_POST["account_no"]."'";
- if(mysqli_query($conn, $query))
- {
-  echo 'data deleted!';
- }
+    $user = '';
+    if (isset($_COOKIE['employer_username'])) {
+        $user = $_COOKIE['employer_username'];
+        $type = "loyer_chequing";
+    } else if (isset($_COOKIE['employee_username'])) {
+        $user = $_COOKIE['employee_username'];
+        $type = "loyee_chequing";
+    }
+    $query = "DELETE FROM `$type` WHERE account_no = '".$_POST["account_no"]."' AND user_name = '$user'";
+    if(mysqli_query($conn, $query))
+    {
+    echo 'data deleted!';
+    }
 }
 
 ?>
