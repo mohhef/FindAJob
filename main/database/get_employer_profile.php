aa258d51d4476b3625002e9f99d06c13cce25472
Mohamed Hefny <mohhef@rocketmail.com>
2020-08-06 17:58:46 +0300

    Fixed update based on schema modification

--- main/database/get_employer_profile.php
+++ main/database/get_employer_profile.php
@@ -5,7 +5,7 @@ function getProfile(){
       require('config.php');
       if($_POST["id"]) {
       $sqlQuery = "SELECT e.user_name, e.company_name, c.telephone_number  FROM employer e,
-contact c WHERE e.user_name = c.euser_name and e.user_name = '".$_POST["id"]."'";
+contact_info c WHERE e.user_name = c.user_name and e.user_name = '".$_POST["id"]."'";
       $result = mysqli_query($conn, $sqlQuery);
       $row = mysqli_fetch_array($result);
       echo json_encode($row);
