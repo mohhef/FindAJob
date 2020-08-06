6af33942a8655e9fd2b237026ef10b1306526226
Robert <robertbeaudenon@gmail.com>
2020-08-06 21:08:33 +0300

    Added profile page to employee + update option

--- main/database/fetch_logged_in_employee.php
+++ main/database/fetch_logged_in_employee.php
@@ -1,3 +1,4 @@
+
 <?php
 
 require("config.php");
@@ -5,16 +6,14 @@ $loggedin_user = $_POST['id'];
 
 $columns = array('user_name', 'company_name', 'telephone_number');
 
-$query1= "SELECT e.user_name, e.company_name, c.telephone_number  FROM employer e,
-contact_info c WHERE e.user_name = c.user_name and e.user_name = '".$loggedin_user."'";
+$query1= "SELECT a.user_name, a.email  FROM all_user a WHERE a.user_name = '".$loggedin_user."'";
 
 
 $result = mysqli_query($conn, $query1); 
 
 function get_all_data($conn)
 {
-  $query = "SELECT e.user_name, e.company_name, c.telephone_number  FROM employer e,
-contact_info c WHERE e.user_name = c.user_name and e.user_name = '".$_POST['id']."'";
+   $query = "SELECT a.user_name, a.email  FROM all_user a WHERE a.user_name = '".$_POST['id']."'";
   $result = mysqli_query($conn, $query);
   return mysqli_num_rows($result);
 }
@@ -28,8 +27,7 @@ while($row = mysqli_fetch_array($result))
 {
 $sub_array = array();
 $sub_array[] = '<div contenteditable="false" class="update" data-id="'.$row["user_name"].'" data-column="user_name">' . $row["user_name"] . '</div>';
-$sub_array[] = '<div contenteditable="false" class="update" data-id="'.$row["user_name"].'" data-column="title">' . $row["company_name"] . '</div>';
-$sub_array[] = '<div contenteditable="false" class="update" data-id="'.$row["user_name"].'" data-column="employee_needed">' . $row["telephone_number"] . '</div>';
+$sub_array[] = '<div contenteditable="false" class="update" data-id="'.$row["user_name"].'" data-column="title">' . $row["email"] . '</div>';
 $sub_array[] = '<button type="button" name="delete" class="btn btn-danger btn-xs delete" id="'.$row["user_name"].'" >Delete</button>';
 $sub_array[] = '<button type="button" name="update" class="btn btn-primary btn-xs update" id="'.$row["user_name"].'" >Update</button>';
 $data[] = $sub_array;
---
+++ main/employee/profile.php
@@ -0,0 +1,168 @@
+<?php include "header.php" ?>
+
+</br></br>
+</br>
+
+<div class="container-fluid" id="rcorners1">
+  <div class="table-responsive">
+    <div id="alert_message"></div>
+    <table id="profile_data" class="display nowrap table table-borderless table-striped" style="" >
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Email</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</div>
+<div id="profileModal" class="modal fade">
+  <div class="modal-dialog modal-lg">
+    <form method="post" id="profileForm" autocomplete="off">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" align="left">Edit Profile</h4>
+        </div>
+        <div class="modal-body">
+          <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2 form-group">
+          <label for="name" class="control-label">Employee Name</label>
+          <input readonly type="text" class="form-control disabled" id="empID" name="empID" placeholder="employeeID">
+        </div>
+
+
+        <div class="col-4 form-group">
+          <label for="description" class="control-label">Email</label>
+          <textarea   class="form-control"  id="email" name="email"></textarea>
+        </div>
+      </div>
+    </div>
+      <div class="modal-footer">
+        <input type="hidden" name="jobid" id="jobid" />
+        <input type="hidden" name="action" id="action" value="" />
+        <input type="submit" name="save" id="save" class="btn btn-info" value="Save" />
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </form>
+</div>
+</div>
+
+<nav id="nav" class="navbar navbar-expand-sm  navbar-dark">
+</nav>
+<?php include "footer.php" ?>
+
+<script type="text/javascript">
+
+var id = getCookie("employee_username");
+console.log(id);
+
+$(document).ready(function(){
+  // document.body.style.zoom="100%"
+  fetch_data();
+  function fetch_data(){
+    var dataTable = $('#profile_data').DataTable({
+        "bootstrap": true,
+      "sDom": "Rlfrtip",
+      "bAutoWidth":false,
+      "responsive":true,
+      "scrollY": "660px",
+      "scrollX": true,
+      "scrollCollapse": true,
+      "processing":true,
+      "serverSide":true,
+      "aLengthMenu": [[10, 25, 30, -1], [10, 25, 30, "All"]],
+      "pageLength": 25,
+      "bInfo":false,
+      //    "order" : [],
+      "columns": [{
+        "orderable": true,
+        // "width":"25px"
+
+      }, {
+        "orderable": true,
+        //  "width":"15%"
+      }, {
+        "orderable": true,
+        //  "width":"15%"
+      }, {
+        "orderable": true,
+        //  "width":"15%"
+      }
+    ],
+    "language": {
+      "sSearch": "  ",
+      "searchPlaceholder": "search"
+    },
+    "ajax" : {
+      url:"../database/fetch_logged_in_employee.php",
+      type:"POST",
+      data:{id:id}
+    }
+  });
+}
+
+$("#profile_data").on('click', '.update', function(){
+  var id = $(this).attr("id");
+  var action = 'getPipeline';
+  $.ajax({
+    url:'../database/get_employee_profile.php',
+    method:"POST",
+    data:{id:id, action:action},
+    dataType:"json",
+    success:function(data){
+      $('#profileModal').modal('show');
+      $('#empID').val(data.user_name);
+      $('#email').val(data.email);
+       $('.modal-title').html("<i class='fa fa-plus'></i> Edit Profile Details");
+       $('#action').val('updateJob');
+       $('#save').val('Save');
+    }
+  });
+});
+
+
+$("#profileModal").on('submit','#profileForm', function(event){
+  event.preventDefault();
+  $('#save').attr('disabled','disabled');
+  var formData = $(this).serialize();
+  console.log(formData)
+  var email = jQuery('input[name="email"]').val();
+   $.ajax({
+    url:"../database/update_profile_employee.php",
+    method:"POST",
+    data:formData,
+    success:function(data){
+      $('#profileForm')[0].reset();
+      $('#profileModal').modal('hide');
+      $('#save').attr('disabled', false);
+      $('#profile_data').DataTable().ajax.reload(null,false);
+      console.log(formData);
+    }
+  });
+});
+
+
+});
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+</script>
+<?php include "footer.php"?>
