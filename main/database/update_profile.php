390d4a9f4da66fc186e1a7e6d41398cfea4286f9
Robert <robertbeaudenon@gmail.com>
2020-08-06 17:58:46 +0300

    Fixed update based on schema modification

--- main/database/update_profile.php
+++ main/database/update_profile.php
@@ -8,13 +8,13 @@ echo($_POST["empID"]);
 
 // $updateContactInfoTable = "UPDATE `contact_info` SET telephone_number ='".$_POST["telephoneNumber"]."' WHERE telephone_number= '".$_POST["empID"]."'";
 // $removeKeyConstraints = "ALTER  TABLE `contact` WITH NOCHECK CONSTRAINT ALL"
- // $updateTelphoneNumberQuery= "UPDATE `contact` SET telephone_number ='".$_POST["telephoneNumber"]."' WHERE euser_name = '".$_POST["empID"]."'";
+ $updateTelphoneNumberQuery= "UPDATE `contact_info` SET telephone_number ='".$_POST["telephoneNumber"]."' WHERE user_name = '".$_POST["empID"]."'";
  $updateCompnayNameQuery = "UPDATE `employer` SET company_name='".$_POST["companyName"]."' WHERE user_name = '".$_POST["empID"]."'";
  //ask should we delete the contact info table 
  // $updateContactInfoTable = "UPDATE `contact_info` SET telephone_number ='".$_POST["telephoneNumber"]."' WHERE telephone_number= '".$_POST["empID"]."'";
 
 // mysqli_query($conn,  $removeKeyConstraints);
-// mysqli_query($conn,  $updateTelphoneNumberQuery);
+mysqli_query($conn,  $updateTelphoneNumberQuery);
 mysqli_query($conn,   $updateCompnayNameQuery);
 
  ?>
