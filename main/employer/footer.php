cd2080599417687aa7de97bee3c152f949ce4a67
Robert <robertbeaudenon@gmail.com>
2020-08-07 13:16:49 +0300

    Redirected any disactivated user to the sibscription page like i did for frozen accounts

--- main/employer/footer.php
+++ main/employer/footer.php
@@ -41,6 +41,36 @@ window.onload = function() {
    })
   };
 
+  window.onload = function() {
+    var id = getCookie("employer_username");
+    $.ajax({
+    url:'../database/get_deactivated_account.php',
+    method:"POST",
+    data:{id:id},
+    error: function(xhr, error){
+        console.log(xhr); console.log(error);
+    },
+    success:function(data){
+      
+      if(data == 2){
+       
+        if ( document.URL.includes("subscription.php") || document.URL.includes("payment.php")  || document.URL.includes("chequing_table.php")  || document.URL.includes("card_table.php")) {
+        }else{
+         redirect_to_subscription();
+        }
+        
+          var elements = document.getElementsByClassName("frozen");
+          console.log(elements.length);
+          
+           elements[0].style.display = 'none';
+           elements[1].style.display = 'none';
+           elements[2].style.display = 'none';
+           elements[3].style.display = 'none';
+           elements[4].style.display = 'none';   
+      } 
+    }
+   })
+  };
 
 function getCookie(cname) {
   var name = cname + "=";
