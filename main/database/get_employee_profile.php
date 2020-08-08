150e99f5f2d9f840d074affd8401cf975901ca7d
Mohamed <mohhef@gmail.com>
2020-08-08 18:55:27 -0400

    fix employee update

--- main/database/get_employee_profile.php
+++ main/database/get_employee_profile.php
@@ -4,7 +4,7 @@ getProfile();
 function getProfile(){
       require('config.php');
       if($_POST["id"]) {
-      $sqlQuery = "SELECT a.user_name, a.email a.telephone_number FROM all_user a WHERE a.user_name = '".$_POST['id']."'";
+      $sqlQuery = "SELECT a.user_name, a.email FROM all_user a WHERE a.user_name = '".$_POST['id']."'";
       $result = mysqli_query($conn, $sqlQuery);
       $row = mysqli_fetch_array($result);
       echo json_encode($row);
