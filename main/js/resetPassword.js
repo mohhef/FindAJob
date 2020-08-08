b9479635b0130bcb40a94e238f5056aa430017f3
Mohamed Hefny <mohhef@rocketmail.com>
2020-08-08 10:39:43 -0400

    added employer forgot password

--- main/js/resetPassword.js
+++ main/js/resetPassword.js
@@ -20,3 +20,26 @@ const employeeResetPassword = () => {
         }
     })
 }
+
+const employerResetPassword = () => {
+    const password = $('input[id=employer_reset_password]').val();
+    const url = window.location;
+    const token = new URLSearchParams(url.search).get('token')
+    const json = {
+        "token": token,
+        "password": password,
+    }
+    $.post('./service/auth/employer_reset_password.php', json, function(data){
+
+        if(!data){
+            alert("could not fulfill request.");
+            return;
+        }
+        const res = JSON.parse(data);
+        if(res.result){
+            window.location.replace('index.php')
+        }else{
+            alert("Registration failed.");
+        }
+    })
+}
