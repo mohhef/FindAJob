36e3bebc1473ec0badb8d240fc53ce8d3f3b0239
Mohamed Hefny <mohhef@rocketmail.com>
2020-08-09 21:23:07 -0400

    fix foreign key bugs

--- main/helpers/config.php
+++ main/helpers/config.php
@@ -8,6 +8,11 @@ function connDB()
 	$password = "m1h2c3r4";
 	$database = "vxc353_1";
 
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$database = "web_career";
+	
 	$conn = mysqli_connect($servername, $username, $password, $database);
 
 //check connection
